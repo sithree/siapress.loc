094a869b97330c11e1b356bb322dc2af35adeadc
streltsov <adminbank@mail.ru>
2014-04-04 16:20:15 +0600

    Merge origin/master
    Conflicts:
    	css/template.css

--- protected/components/widgets/Blogs.php
+++ protected/components/widgets/Blogs.php
@@ -6,7 +6,9 @@ class Blogs extends CPortlet {
     protected function renderContent() {
 //        $model = Yii::app()->cache->get('blogsRight');
 //        if ($model === false) {
-            $model = Article::model()->blogsRight()->findAll();
+            $model = Article::model()->blogsRight()->findAll(
+                    
+                    );
 //            Yii::app()->cache->set('blogsRight', $model, Config::getCacheduration());
 //        }
         $this->render('blogs', array('model' => $model));
